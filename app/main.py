de357f94a83eca2abd6274392e5dd8d49c961063
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-08-21 10:22:08 +0500

    add CORS middleware.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # import psycopg2
 # from psycopg2.extras import RealDictCursor
 from .database import engine
@@ -11,6 +12,18 @@ from .config import settings
 
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(posts.router)
